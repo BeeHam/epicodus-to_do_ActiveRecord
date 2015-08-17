da257273a293fd7e8fee593500af063bde933def
Janice Dunn & Janice Laset-Parkerson <student@epicodus.com>
2015-08-17 10:47:12 -0700

    Add spec to check if two tasks are the same.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,9 +1,22 @@
 require('rspec')
 require('task')
+require('pg')
+
+DB = PG.connect({:dbname => 'to_do_test'})
+
+RSpec.conigure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM tasks *")
+  end
+end
 
 describe(Task) do
-  before() do
-    Task.clear()
+  describe("#==") do
+    it("is the same task if it has the same description") do
+      task1 = Task.new({:description => "learn SQL"})
+      task2 = Task.new({:description => "learn SQL"})
+      expect(task1).to(eq(task2))
+    end
   end
 
   describe("#description") do
