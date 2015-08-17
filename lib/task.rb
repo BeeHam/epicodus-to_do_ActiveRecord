58b7639f3555651d17b2ee3c72440568e408bc97
Janice Dunn & Janice Laset-Parkerson <student@epicodus.com>
2015-08-17 16:47:09 -0700

    add sort

--- lib/task.rb
+++ lib/task.rb
@@ -1,9 +1,11 @@
 class Task
-  attr_reader(:description, :list_id)
+  attr_reader(:description, :list_id, :due_date, :id)
 
   define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
     @list_id = attributes.fetch(:list_id)
+    @due_date = attributes.fetch(:due_date)
+    @id = attributes.fetch(:id)
   end
 
   define_method(:description) do
@@ -16,13 +18,15 @@ class Task
     returned_tasks.each() do |task|
       description = task.fetch("description")
       list_id = task.fetch("list_id").to_i()
-      tasks.push(Task.new({:description => description, :list_id => list_id}))
+      due_date = task.fetch("due_date")
+      id = task.fetch("id").to_i()
+      tasks.push(Task.new({:description => description, :list_id => list_id, :due_date => due_date, :id => id}))
     end
     tasks
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
+    DB.exec("INSERT INTO tasks (description, list_id, due_date) VALUES ('#{@description}', #{@list_id}, '#{@due_date}');")
   end
 
   define_singleton_method(:clear) do
@@ -30,7 +34,18 @@ class Task
   end
 
   define_method(:==) do |another_task|
-    self.list_id().==another_task.list_id()
+    self.id().==another_task.id()
   end
 
+  define_singleton_method(:sort) do
+    returned_tasks = DB.exec("SELECT * FROM tasks ORDER BY due_date;")
+    tasks = []
+    returned_tasks.each() do |task|
+      description = task.fetch("description")
+      list_id = task.fetch("list_id").to_i()
+      due_date = task.fetch("due_date")
+      tasks.push(Task.new({:description => description, :list_id => list_id, :due_date => due_date}))
+    end
+    tasks
+  end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -14,15 +14,15 @@ require('spec_helper')
 describe(Task) do
   describe("#==") do
     it("is the same task if it has the same description") do
-      task1 = Task.new({:description => "learn SQL", :list_id => 1})
-      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      task1 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2015-08-31', :id => nil})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2015-08-31', :id => nil})
       expect(task1).to(eq(task2))
     end
   end
 
   describe("#description") do
     it("lets you read the description out") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2015-08-31', :id => nil})
       expect(test_task.description()).to(eq("learn SQL"))
     end
   end
@@ -33,9 +33,17 @@ describe(Task) do
     end
   end
 
+  describe("#id") do
+    it("sets its ID when you save it") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2015-08-31', :id => nil})
+      test_task.save()
+      expect(test_task.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2015-08-31', :id => nil})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
@@ -43,7 +51,7 @@ describe(Task) do
 
   describe(".clear") do
     it("empties out all of the saved tasks") do
-      Task.new({:description => "learn SQL", :list_id => 1}).save()
+      Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2015-08-31', :id => nil}).save()
       Task.clear()
       expect(Task.all()).to(eq([]))
     end
@@ -51,9 +59,29 @@ describe(Task) do
 
   describe("#list_id") do
     it("lets you read the list ID out") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2015-08-31', :id => nil})
       expect(test_task.list_id()).to(eq(1))
     end
   end
 
+  describe("#due_date") do
+    it("lets you read the due date out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2015-08-31', :id => nil})
+      expect(test_task.due_date()).to(eq('2015-08-31'))
+    end
+  end
+
+  describe('.sort') do
+    it('lets you sort by due date') do
+      task1 = Task.new({:description => "learn SQL", :list_id => 2, :due_date => '2015-08-31', :id => nil})
+      task1.save()
+      task2 = Task.new({:description => "prepare lunch", :list_id => 1, :due_date => '2015-08-18', :id => nil})
+      task2.save()
+      task3 = Task.new({:description => "cook dinner", :list_id => 4, :due_date => '2015-08-10', :id => nil})
+      task3.save()
+      task4 = Task.new({:description => "homework", :list_id => 2, :due_date => '2015-08-15', :id => nil})
+      task4.save()
+      expect(Task.()).to(eq([task3, task4, task2, task1]))
+    end
+  end
 end
